76e2f54281ba30a30d65df11f475b6a95b9707f4
sawfiz <dashu.up@gmail.com>
2023-11-10 09:23:28 +0800

    Add create author form

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -3,6 +3,7 @@ const Book = require('../models/book');
 const BookInstance = require('../models/bookinstance');
 const asyncHandler = require('express-async-handler');
 const validateObjectId = require('../middleware/validateObjectId');
+const { body, validationResult } = require('express-validator');
 
 // Display list of all Authors.
 exports.author_list = asyncHandler(async (req, res, next) => {
@@ -16,7 +17,7 @@ exports.author_detail = [
   asyncHandler(async (req, res, next) => {
     const [author, authoredBooks] = await Promise.all([
       Author.findById(req.params.id).exec(),
-      Book.find({ author: req.params.id }, "title summary").exec(),
+      Book.find({ author: req.params.id }, 'title summary').exec(),
     ]);
 
     if (author === null) {
@@ -30,14 +31,56 @@ exports.author_detail = [
 ];
 
 // Display Author create form on GET.
-exports.author_create_get = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Author create GET');
-});
+exports.author_create_get = (req, res, next) => {
+  res.render('author_form', { title: 'Create Author' });
+};
 
 // Handle Author create on POST.
-exports.author_create_post = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Author create POST');
-});
+exports.author_create_post = [
+  body('first_name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('First name must be specified')
+    .isAlphanumeric()
+    .withMessage('First name has non-alphanumeric charecters'),
+  body('family_name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Family name must be specified')
+    .isAlphanumeric()
+    .withMessage('First name has non-alphanumeric charecters'),
+  body('date_of_birth', 'Invalid date of birth')
+    .optional({ values: 'falsy' })
+    .isISO8601()
+    .toDate(),
+  body('date_of_death', 'Invalid date of death')
+    .optional({ values: 'falsy' })
+    .isISO8601()
+    .toDate(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render('author_form', {
+        title: 'Create Author',
+        author,
+        errors: errors.array(),
+      });
+    } else {
+      await author.save();
+      res.redirect(author.url);
+    }
+  }),
+];
 
 // Display Author delete form on GET.
 exports.author_delete_get = asyncHandler(async (req, res, next) => {
--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,7 +1,8 @@
 const Genre = require('../models/genre');
 const Book = require('../models/book');
 const asyncHandler = require('express-async-handler');
-const validateObjectId = require('../middleware/validateObjectId')
+const validateObjectId = require('../middleware/validateObjectId');
+const { body, validationResult } = require('express-validator');
 
 // Display list of all Genre.
 exports.genre_list = asyncHandler(async (req, res, next) => {
@@ -27,14 +28,46 @@ exports.genre_detail = [
 ];
 
 // Display Genre create form on GET.
-exports.genre_create_get = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Genre create GET');
-});
+// Display Genre create form on GET.
+exports.genre_create_get = (req, res, next) => {
+  res.render('genre_form', { title: 'Create Genre' });
+};
 
 // Handle Genre create on POST.
-exports.genre_create_post = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Genre create POST');
-});
+exports.genre_create_post = [
+  // Validate and sanitize the name field
+  body('name', 'Genre name must contain at least 3 characters')
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  // Process request after validation and sanitation
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const genre = new Genre({ name: req.body.name });
+
+    if (!errors.isEmpty()) {
+      // There are errors, render the form again with sanitized values and error messages.
+      res.render('genre_form', {
+        title: 'Create Genre',
+        genre,
+        errors: errors.array(),
+      });
+    } else {
+      // Data from from is valid.
+      // Check if Genre with the same name already exists
+      const genreExists = await Genre.findOne({ name: req.body.name })
+        .collation({ locale: 'en', strength: 2 })
+        .exec();
+      if (genreExists) {
+        res.redirect(genreExists.url);
+      } else {
+        await genre.save();
+        res.redirect(genre.url);
+      }
+    }
+  }),
+];
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = asyncHandler(async (req, res, next) => {
